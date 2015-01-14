412aba2cfda8712ee2171a07b4f84368cebfc9df
Mike Kohn <kohnmd@gmail.com>
2015-01-13 21:45:36 -0700

    First styles, early support for gutsy-nudge shortcode.

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * Chillout child theme style includes.
+ */
 function a13_child_style(){
     global $wp_styles;
 
@@ -16,6 +19,86 @@ function a13_child_style(){
 }
 add_action('wp_enqueue_scripts', 'a13_child_style',11);
 
-/*
- * Add here your functions below, and overwrite native theme functions
+
+/**
+ * Add your functions below, and overwrite native theme functions.
  */
+
+
+// Create "Nudge" post type.
+add_action( 'init', 'nudge_init' );
+function nudge_init() {
+	$args = array(
+		'labels'                => array(
+    		'name'          => 'Nudges',
+    		'singular_name' => 'Nudge'
+    	),
+		'public'                => true,
+		'exclude_from_search'   => true,
+		'show_in_nav_menus'     => false,
+		'show_in_menu'          => true,
+		'query_var'             => true,
+		'rewrite'               => array( 'slug' => 'nudge' ),
+		'taxonomies'            => array( 'category' ),
+		'supports'              => array( 'title', 'author' )
+	);
+	register_post_type( 'nudge', $args );
+}
+
+
+// Add a default category to nudges if none is specified.
+add_action('publish_nudge', 'nudge_default_category');
+function nudge_default_category($post_id) {
+	global $wpdb;
+	if(!has_term('','category',$post_id)){
+		$cats = array(1); // "Random"
+		wp_set_object_terms($post_id, $cats, 'category');
+	}
+}
+
+
+// Create shortcode for nudges.
+add_shortcode( 'gutsy-nudge', 'gutsy_nudge_dropdown' );
+function gutsy_nudge_dropdown() {
+	$categories = get_categories(array(
+    	'type'          => 'nudge',
+    	'orderby'       => 'name',
+    	'order'         => 'ASC',
+    	'hide_empty'    => 1
+    ));
+    
+    $output = '<div id="nudge">';
+    
+        $output = '<select id="nudge-category">';
+            $output .= '<option value="">Where do you need a nudge?</option>';
+        	foreach ($categories as $category) {
+            	$output .= '<option value="' . $category->term_id . '">' . $category->name . '</option>';
+        	}
+        $output .= '</select>';
+	
+	$output .= '<div id="nudge-text-container"></div>';
+	
+	return $output;
+}
+
+
+
+
+
+/**
+ * Debugger
+ */
+
+function pre_print($var, $title="", $return=false) {
+	$output = "";
+	$output .= ($title) ? '<strong>'.$title.'</strong>' : "";
+	$output .= '<pre>';
+		$output .= print_r($var, true);
+	$output .= '</pre>';
+	
+	if($return) {
+		return $output;
+	} else {
+		echo $output;
+	}
+}
