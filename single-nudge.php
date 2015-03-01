e59e9e8ee1bd3d7fc2cf6eae6b1a6b878dffc787
Mike Kohn <kohnmd@gmail.com>
2015-03-01 12:28:35 -0700

    Add category name and links to nudge single page

--- single-nudge.php
+++ single-nudge.php
@@ -8,16 +8,35 @@ global $a13_option;
 global $content_width;
 
 
-get_header(); 
+get_header();
 ?>
 
 <article id="content" class="clearfix">
 
     <?php while (have_posts()) : the_post(); ?>
+        
         <?php
-        //pre_print(wp_get_post_categories());
+        $primary_post_category = null;
+        
+        $post_terms = get_the_terms(get_the_ID(), 'category');
+        $i = 0;
+        foreach($post_terms as $term) {
+            if (isset($_GET['c'])) {
+                if ($_GET['c'] == $term->term_id) {
+                    $primary_post_category = $term;
+                    break;
+                }
+            }
+            
+            if (++$i == 1) {
+                $primary_post_category = $term;
+                if (!isset($_GET['c'])) {
+                    break;
+                }
+            }
+        }
         ?>
-
+        
         <div id="col-mask">
             <div id="post-<?php the_ID(); ?>" <?php post_class('post-content'); ?>>
                 <div class="real-content">
@@ -34,8 +53,13 @@ get_header();
                                 <div class="nudge-wrapper">
                                     <h2 class="page-title">
                                         Your 
-                                        <span class="stylish-font">Gutsy Nudge</span> for 
-                                        <span class="nudge-category stylish-font"><?php // MIKE ?></span>
+                                        <span class="stylish-font">Gutsy Nudge</span>
+                                        <?php if ($primary_post_category) { ?>
+                                            for 
+                                            <span class="nudge-category stylish-font">
+                                                <?php echo $primary_post_category->name; ?>
+                                            </span>
+                                        <?php } ?>
                                     </h2>
                                     <div id="nudge-description">
                                         <?php the_field('nudge_description'); ?>
@@ -45,7 +69,11 @@ get_header();
                                             ?>
                                             <p>
                                                 <a href="<?php echo $blog_post_link['url']; ?>">
-                                                    Read more about <?php // MIKE ?> on our blog...
+                                                    Read more
+                                                    <?php if ($primary_post_category) { ?>
+                                                        about <?php echo $primary_post_category->name; ?>
+                                                    <?php } ?>
+                                                    on our blog.
                                                 </a>
                                             </p>
                                             <?php
@@ -56,7 +84,7 @@ get_header();
                                 
                                 <div class="vc_row wpb_row vc_inner vc_row-fluid">
                                     <div class="vc_col-sm-4 wpb_column vc_column_container a13-sc-button_wrapper">
-                                        <a class="a13-sc-button  vc_btn vc_btn_blue vc_btn_md vc_btn_rounded another-nudge-button" title="" href="<?php home_url(); ?>">
+                                        <a class="a13-sc-button  vc_btn vc_btn_blue vc_btn_md vc_btn_rounded another-nudge-button" title="" href="<?php echo home_url(); ?>">
                                             <span>Get Another Nudge</span>
                                         </a>
                                     </div>
@@ -66,8 +94,11 @@ get_header();
                                         </a>
                                     </div>
                                     <div class="vc_col-sm-4 wpb_column vc_column_container a13-sc-button_wrapper">
-                                        <a class="a13-sc-button  vc_btn vc_btn_blue vc_btn_md vc_btn_rounded blog-post-button" title="" href="http://highhearthealing.com/blog/<?php // MIKE ?>">
-                                            Blog Posts on Courage
+                                        <a class="a13-sc-button  vc_btn vc_btn_blue vc_btn_md vc_btn_rounded blog-post-button" title="" href="http://highhearthealing.com/blog/<?php if ($primary_post_category) { echo $primary_post_category->slug; } ?>">
+                                            Blog Posts
+                                            <?php if ($primary_post_category) { ?>
+                                                on <?php echo $primary_post_category->name; ?>
+                                            <?php } ?>
                                         </a>
                                     </div>
                                 </div>
